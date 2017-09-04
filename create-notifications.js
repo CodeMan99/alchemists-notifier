673ed9aa78ac2c9ac2ba5c11982e196484f4b4dc
Cody Taylor <codemister99@yahoo.com>
2017-09-04 00:46:27 -0400

    Make icons sub-directory.

--- create-notifications.js
+++ create-notifications.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(message => {
 	chrome.notifications.create(null, {
 		type: 'basic',
-		iconUrl: chrome.extension.getURL('alc-16.png'),
+		iconUrl: chrome.extension.getURL('icons/alc-16.png'),
 		title: 'Alchemists',
 		message: message
 	});
