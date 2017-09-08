3eb1ac247e404ada818c13021932af9d48706fc4
Cody Taylor <codemister99@yahoo.com>
2017-09-08 14:20:45 -0400

    Clicking the notification will focus the game.

--- create-notifications.js
+++ create-notifications.js
@@ -1,8 +1,19 @@
-chrome.runtime.onMessage.addListener(message => {
-	chrome.notifications.create(null, {
-		type: 'basic',
+chrome.runtime.onMessage.addListener((message, sender) => {
+	var id = `${sender.tab.id}-${sender.tab.windowId}-${Date.now()}`;
+
+	chrome.notifications.create(id, {
 		iconUrl: chrome.extension.getURL('icons/alc-16.png'),
+		isClickable: true,
+		message: message,
 		title: 'Alchemists',
-		message: message
+		type: 'basic'
+	});
+});
+
+chrome.notifications.onClicked.addHandler(id => {
+	var [tabId, windowId, _/* creation */] = id.split('-');
+
+	chrome.tabs.update(tabId, {active: true}, () => {
+		chrome.windows.update(windowId, {focused: true});
 	});
 });
